b47b1a403555077e6df346ee270ce2f67ea1f647
adnanesaber15 <adnanesaber15@gmail.com>
2021-05-15 02:45:00 +0000

    testing the git idea

--- root/views.py
+++ root/views.py
@@ -146,6 +146,7 @@ def update_view(request):
             context = out
         else:
             context = "<span style='color: #ccc'>Error</span>"
+            err = ""
         return render(request, "git_update.html", context={'output': context,'error':err})
     else:
         raise Http404
