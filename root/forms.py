5699fc109182504477d27e19c594b69399dae69d
adnanesaber15 <adnanesaber15@gmail.com>
2021-05-22 09:08:11 +0100

    testing merge

--- root/forms.py
+++ root/forms.py
@@ -1,8 +1,24 @@
 from django import forms
 from .models import Lead
+from django.utils.translation import ugettext_lazy as _
 
 
 class ContactForm(forms.ModelForm):
     class Meta:
         model = Lead
         fields = "__all__"
+        labels = {
+            'firstName': _('First name'),
+            'lastName': _('Last name'),
+            'mail': _('Mail address'),
+            'phone': _('Phone number'),
+            'interestedBy': _('Intereseted by'),
+            'message': _('Your message'),
+        }
+        widgets = {
+            'firstName': forms.TextInput(attrs={'placeholder': 'First name'}),
+            'phone': forms.TextInput(attrs={'placeholder': 'Phone number'}),
+            'lastName': forms.TextInput(attrs={'placeholder': 'Last name'}),
+            'mail': forms.TextInput(attrs={'placeholder': 'Mail address'}),
+            'message': forms.Textarea(attrs={'placeholder': 'Type your message...'}),
+        }
